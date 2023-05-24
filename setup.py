5ed5311bc83f1d541c713f997b73a5874094fe48
Elielson Andre <elielsonandre123@gmail.com>
2023-05-24 01:06:31 -0300

    chrore: setting up application

--- setup.py
+++ setup.py
@@ -1,3 +1,36 @@
 from view.SplashScren import SplashScren
 from view.Text import Text
+from engine.Runner import Runner
+import threading
+import os
+
 SplashScren.show()
+
+print("Insira o endereço IP alvo")
+ip = str(input(Text.cyan("Digite o IP => ")))
+os.system('clear')
+
+print(f"Insira a porta Ex: 8080 \n(Se não passar nada a padrão é {Text.yellow('80')})")
+port = str(input(Text.cyan("Porta => ")))
+os.system('clear')
+
+print(f"Quantidade de pacotes na injeção Ex: 120 \n(Se não passar nada por padrão vai ser {Text.yellow('100')})")
+pack = str(input(Text.cyan("Quantidade Pacotes => ")))
+os.system('clear')
+
+print(f"Threads Ex: 120 \n(Se não passar nada por padrão vai ser {Text.yellow('20')})")
+thread = str(input(Text.cyan("Threads => ")))
+os.system('clear')
+
+#---- Passando valores padrao se vier vazio
+if port == '':
+    port = 80
+if pack == '':
+    pack = 100
+if thread == '':
+    thread = 20
+
+run = Runner()
+for x in range(thread):
+    thred = threading.Thread(target=run.start(ip, port, pack, thread))
+    thred.start()
--- view/Text.py
+++ view/Text.py
@@ -2,22 +2,16 @@ from colorama import Fore, Style
 import time
 class Text:
 
-    def input(message):
-        return Fore.CYAN + message + " => " + Style.RESET_ALL
-    
-    
-     # Text colors ---------------------
-    def txt_blue(text):
-        return Fore.BLUE + str(text) + Style.RESET_ALL
-    
-    def txt_red(text):
+    # Text colors ---------------------
+
+    def red(text):
         return Fore.RED + str(text) + Style.RESET_ALL
     
-    def txt_yellow(text):
+    def yellow(text):
         return Fore.YELLOW + str(text) + Style.RESET_ALL
     
-    def txt_green(text):
+    def green(text):
         return Fore.GREEN + str(text) + Style.RESET_ALL
     
-    def txt_cyan(text):
+    def cyan(text):
         return Fore.CYAN + str(text) + Style.RESET_ALL
